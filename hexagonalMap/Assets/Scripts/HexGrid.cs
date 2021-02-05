9ae1f0791f56a062123098cf023d169a86e1a4d5
Jonathan Harrison <jonathan.harrison@hotmail.co.uk>
2021-02-05 13:57:03 +0000

    Branch for hexagonal map version of game

---
+++ hexagonalMap/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public Transform[] hexagonPrefabs = new Transform[3];
+    public int gridWidth;
+    public int gridHeight;
+    public int gridDepth;
+    public int noiseScale;
+    public AnimationCurve noiseCurve;
+
+    private Texture2D perlinTexture;
+
+    float hexWidth = 1.732f;
+    float hexHeight = 2f;
+    public float gap = 0.0f;
+
+    Vector3 startPos;
+    void Start()
+    {
+        CreatePerlinTexture();
+        AddGap();
+        CalcStartPos();
+        CreateGrid();
+    }
+
+    void CreatePerlinTexture()
+    {
+        PerlinGenerator perlin = new PerlinGenerator();
+        perlinTexture = perlin.GenerateTexture();
+    }
+
+    void AddGap()
+    {
+        hexWidth = 1.732f;
+        hexHeight = 2f;
+        hexWidth += hexWidth * gap;
+        hexHeight += hexHeight * gap;
+    }
+
+    void CalcStartPos()
+    {
+        float offset = 0;
+        if (gridHeight / 2 % 2 != 0)
+            offset = hexWidth / 2;
+        float x = -hexWidth * (gridWidth / 2) - offset;
+        float z = hexHeight * 0.75f * (gridHeight / 2);
+        startPos = new Vector3(x, 0, z);
+    }
+
+    Vector3 CalcWorldPos(Vector2 gridPos)
+    {
+        float offset = 0;
+        if (gridPos.y % 2 != 0)
+            offset = hexWidth / 2;
+        float x = startPos.x + gridPos.x * hexWidth + offset;
+        float z = startPos.z - gridPos.y * hexHeight * 0.75f;
+        return new Vector3(x, 0.0f, z);
+    }
+
+    float addNoise(Vector2 gridPos)
+    {
+        float offset = 0;
+        if (gridPos.y % 2 != 0)
+            offset = hexWidth / 2;
+        float xNoise = startPos.x + gridPos.x * hexWidth + offset;
+        float zNoise = startPos.z - gridPos.y * hexHeight * 0.75f;
+        float yNoise = perlinTexture.GetPixel(Mathf.FloorToInt(xNoise), Mathf.FloorToInt(zNoise)).grayscale;
+        return yNoise;
+    }
+
+    void CreateGrid()
+    {
+        float[,] heights = new float[gridWidth, gridHeight];
+        float minHeight = 9999f, maxHeight = 0f;
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                Vector2 gridPos = new Vector2(x, y);
+                float yNoise = addNoise(gridPos);
+                float scaleChange = gridDepth + (yNoise * noiseScale * noiseCurve.Evaluate(yNoise * noiseScale));
+                heights[x, y] = scaleChange;
+                if (scaleChange <= minHeight)
+                    minHeight = scaleChange;
+                if (scaleChange >= maxHeight)
+                    maxHeight = scaleChange;
+            }
+        }
+
+        float range = maxHeight - minHeight;
+        float ranges = range / hexagonPrefabs.Length;
+
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                Vector2 gridPos = new Vector2(x, y);
+
+                Transform hex;
+                if (heights[x, y] >= minHeight && heights[x, y] < minHeight + ranges)
+                    hex = Instantiate(hexagonPrefabs[0]) as Transform;
+                else if (heights[x, y] >= minHeight + ranges && heights[x, y] <= maxHeight - ranges) 
+                    hex = Instantiate(hexagonPrefabs[1]) as Transform;
+                else
+                    hex = Instantiate(hexagonPrefabs[2]) as Transform;
+
+                hex.position = CalcWorldPos(gridPos);
+                Vector3 scale = new Vector3(0.0f, heights[x, y], 0.0f);
+                hex.localScale += scale;
+                hex.parent = this.transform;
+                hex.name = "Hexagon" + x + "," + y;
+            }
+        }
+    }
+
+}
---
+++ hexagonalMap/Assets/Scripts/PerlinGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class PerlinGenerator 
+{
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20f;
+
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    void Start()
+    {
+        offsetX = UnityEngine.Random.Range(0f, 99999f);
+        offsetY = UnityEngine.Random.Range(0f, 99999f);
+    }
+
+    public Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
